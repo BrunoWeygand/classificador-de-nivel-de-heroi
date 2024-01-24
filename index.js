effd34150ea325614481b4d256f19e7d2c0301b8
Bruno Oliveira <157451148+BrunoWeygand@users.noreply.github.com>
2024-01-24 16:05:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Variáveis
+const nickHeroi = "Cleyton";
+let xpHeroi = 1000;
+let nivelHeroi;
+
+// Estrutura de decisão para determinar o nível com base na XP
+if (xpHeroi <= 1000) {
+  nivelHeroi = "Ferro";
+} else if (xpHeroi <= 2000) {
+  nivelHeroi = "Bronze";
+} else if (xpHeroi <= 5000) {
+  nivelHeroi = "Prata";
+} else if (xpHeroi <= 7000) {
+  nivelHeroi = "Ouro";
+} else if (xpHeroi <= 8000) {
+  nivelHeroi = "Platina";
+} else if (xpHeroi <= 9000) {
+  nivelHeroi = "Ascendente";
+} else if (xpHeroi <= 10000) {
+  nivelHeroi = "Imortal";
+} else {
+  nivelHeroi = "Radiante";
+}
+
+// Saída
+console.log("O Herói de nome " + nickHeroi + " está no nível de " + nivelHeroi);
